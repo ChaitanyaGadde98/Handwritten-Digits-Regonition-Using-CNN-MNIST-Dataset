b32b2d3231b2292afb09820346536d161767a0ad
CHAITANYAVISHNU GADDE <34119802+iamchaituz@users.noreply.github.com>
2019-04-09 12:31:58 +0530

    Add files via upload

--- predict.py
+++ predict.py
@@ -1,7 +1,25 @@
 import numpy as np
 import cv2
+from keras.models import load_model
+import csv
 
 width, height = 28, 28
 
+model = load_model('mnist_cnn.model')
+
 image = cv2.imread('test/img_1.jpg')
-image = cv2.resize(image, (width, height))
+    
+def img_process(image):
+    image = cv2.resize(image, (width, height))
+    img = np.array(image).reshape((width, height, 3))
+    img = np.expand_dims(img, axis = 0)
+    return img
+
+def prediction(img):
+    prediction = model.predict(img)[0]
+    for i in range(10):
+        if(prediction[i] == 1):
+            return i
+
+print(prediction(img_process(image)))
+
